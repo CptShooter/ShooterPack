4d6147f9a5022a2c93babc0e12e9decd065916ab
CptShooter <bk_cpt_pawel@wp.pl>
2014-05-18 22:35:23 +0200

    v0.5 hotfix
    - version text

--- src/org/bitbucket/cptshooter/shooterpack/Main.java
+++ src/org/bitbucket/cptshooter/shooterpack/Main.java
@@ -17,6 +17,8 @@ import java.io.File;
  */
 public class Main extends javax.swing.JFrame {
 
+    public static final String VERSION = "0.5";
+    
     Authentication authentication;
     WebLink weblink = new WebLink();
     String packServer = "https://dl.dropboxusercontent.com/s/";
@@ -27,6 +29,7 @@ public class Main extends javax.swing.JFrame {
 
     public Main() {        
         initComponents();
+        titleText.setText("ShooterPack v"+VERSION);
         dProgressBar.setVisible(false);
         zProgressBar.setVisible(false);
         logoutButton.setVisible(false);
